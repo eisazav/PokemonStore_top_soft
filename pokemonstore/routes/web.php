2ef1b9332ae4198f4f657389503781565c22cc4b
Josue Daniel Bustamante <josuedanielbustamante@gmail.com>
2022-09-20 15:39:38 -0500

    Add pokemon

--- pokemonstore/routes/web.php
+++ pokemonstore/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cars', 'App\Http\Controllers\CarController@index')->name('cars.index');
+Route::get('/cars/create', 'App\Http\Controllers\CarController@create')->name('cars.create');
+Route::get('/cars/stats', 'App\Http\Controllers\CarController@stats')->name('cars.stats');
+Route::post('/cars/save', 'App\Http\Controllers\CarController@save')->name('cars.save');
