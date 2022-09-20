7e2574fe87b16354574fa1435a8a87ab1e9ff5fb
Josue Daniel Bustamante <josuedanielbustamante@gmail.com>
2022-09-20 15:57:44 -0500

    Add pokemon migration

---
+++ pokemonstore/database/factories/PokemonFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class PokemonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'type' => fake()->text(),
+            'weakness' => fake()->text(),
+            'ablity' => fake()->text(),
+            'height' => fake()->randomNumber(2),
+            'weight' => fake()->randomNumber(2),
+            'description' => fake()->text(),
+            'cost' => fake()->randomNumber(2),
+            'evolution'  => fake()->boolean(),
+            'stat_hp' => fake()->randomNumber(2),
+            'stat_attack' => fake()->randomNumber(2),
+            'stat_defense' => fake()->randomNumber(2),
+            'stat_special_attack' => fake()->randomNumber(2),
+            'stat_special_defense' => fake()->randomNumber(2),
+            'stat_speed' => fake()->randomNumber(2),
+            'of_the_month' => fake()->boolean(),
+        ];
+    }
+}
---
+++ pokemonstore/database/migrations/2022_08_17_164544_create_pokemon_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pokemon', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('type');
+            $table->string('weakness');
+            $table->string('ablity');
+            $table->double('height');
+            $table->double('weight');
+            $table->string('description');
+            $table->double('cost');
+            $table->boolean('evolution');
+            $table->bigInteger('evolutionId')->nullable();
+            $table->integer('stat_hp');
+            $table->integer('stat_attack');
+            $table->integer('stat_defense');
+            $table->integer('stat_special_attack');
+            $table->integer('stat_special_defense');
+            $table->integer('stat_speed');
+            $table->boolean('of_the_month');
+        });
+
+        Schema::table('pokemon', function (Blueprint $table) {
+            $table->foreign('evolutionId')->references('id')->on('pokemon');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pokemon');
+    }
+};
