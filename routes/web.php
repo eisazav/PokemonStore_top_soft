2b7da9322ecc4500ac8945a70a640a8ba8ca6442
Mario Alejandro Muñetón Durango <47578828+mamunetond@users.noreply.github.com>
2022-11-13 04:17:06 -0500

    Actualización del archivo de rutas del admin

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,16 @@ Route::post('/login',[LogInController::class, 'store'])
 Route::get('/logout',[LogInController::class, 'destroy'])
     ->middleware('auth')
     ->name('login.destroy');
+
+Route::middleware('admin')->group(function () {
+    //Admin section
+    Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
+
+    //Admin section CRUD
+    Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name("admin.product.index");
+    Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name("admin.product.store");
+    Route::delete('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@delete')->name("admin.product.delete");
+    Route::get('/admin/products/{id}/edit', 'App\Http\Controllers\Admin\AdminProductController@edit')->name("admin.product.edit");
+    Route::put('/admin/products/{id}/update', 'App\Http\Controllers\Admin\AdminProductController@update')->name("admin.product.update");
+    Route::get('/admin/historyProduct/{id}', 'App\Http\Controllers\Admin\AdminProductHistoryController@index')->name("admin.historyProduct.index");
+});
