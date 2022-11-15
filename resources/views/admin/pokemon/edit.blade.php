98e2a2678ecfc558671581cc5337e53e107f3d62
Mario Alejandro Muñetón Durango <47578828+mamunetond@users.noreply.github.com>
2022-11-15 00:34:50 -0500

    Actualización del editable

--- resources/views/admin/pokemon/edit.blade.php
+++ resources/views/admin/pokemon/edit.blade.php
@@ -2,7 +2,7 @@
 @section('title', $viewData["title"])
 @section('content')
 <div class="card mb-4">
-    <div class="card-header"> Edit Product
+    <div class="card-header"> Edit Pokemon
     </div>
     <div class="card-body">
         @if($errors->any())
@@ -12,7 +12,7 @@
                 @endforeach
             </ul>
         @endif
-        <form method="POST" action="{{ route('admin.product.update', ['id'=> $viewData['product']->getId()]) }}"
+        <form method="POST" action="{{ route('admin.pokemon.update', ['id'=> $viewData['pokemon']->getId()]) }}"
             enctype="multipart/form-data">
             @csrf
             @method('PUT')
@@ -21,15 +21,15 @@
                     <div class="mb-3 row">
                         <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Name:</label>
                         <div class="col-lg-10 col-md-6 col-sm-12">
-                            <input name="name" value="{{ $viewData['product']->getName() }}" type="text" class="form-control">
+                            <input name="name" value="{{ $viewData['pokemon']->getName() }}" type="text" class="form-control">
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="mb-3 row">
-                        <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Brand:</label>
+                        <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Type:</label>
                         <div class="col-lg-10 col-md-6 col-sm-12">
-                            <input name="brand" value="{{ $viewData['product']->getBrand() }}" type="text" class="form-control">
+                            <input name="type" value="{{ $viewData['pokemon']->getType() }}" type="text" class="form-control">
                         </div>
                     </div>
                 </div>
@@ -37,9 +37,25 @@
             <div class="row">
                 <div class="col">
                     <div class="mb-3 row">
-                        <label class="col-lg-4 col-md-6 col-sm-12 col-form-label">Category:</label>
+                        <label class="col-lg-4 col-md-6 col-sm-12 col-form-label">Weakness:</label>
                         <div class="col-lg-8 col-md-6 col-sm-12">
-                            <input name="category" value="{{ $viewData['product']->getCategory() }}" type="text" class="form-control">
+                            <input name="weakness" value="{{ $viewData['pokemon']->getWeakness() }}" type="text" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="mb-3 row">
+                        <label class="col-lg-4 col-md-6 col-sm-12 col-form-label">Ability:</label>
+                        <div class="col-lg-8 col-md-6 col-sm-12">
+                            <input name="ability" value="{{ $viewData['pokemon']->getAbility() }}" type="text" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="mb-3 row">
+                        <label class="col-lg-4 col-md-6 col-sm-12 col-form-label">Height:</label>
+                        <div class="col-lg-8 col-md-6 col-sm-12">
+                            <input name="height" value="{{ $viewData['pokemon']->getHeight() }}" type="text" class="form-control">
                         </div>
                     </div>
                 </div>
@@ -47,15 +63,15 @@
                     <div class="mb-3 row">
                         <label class="col-lg-4 col-md-6 col-sm-12 col-form-label">Weight:</label>
                         <div class="col-lg-8 col-md-6 col-sm-12">
-                            <input name="weight" value="{{ $viewData['product']->getWeight() }}" type="text" class="form-control">
+                            <input name="weight" value="{{ $viewData['pokemon']->getWeight() }}" type="text" class="form-control">
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="mb-3 row">
-                        <label class="col-lg-4 col-md-6 col-sm-12 col-form-label">Price:</label>
+                        <label class="col-lg-4 col-md-6 col-sm-12 col-form-label">Cost:</label>
                         <div class="col-lg-8 col-md-6 col-sm-12">
-                            <input name="price" value="{{ $viewData['product']->getPrice() }}" type="number" class="form-control">
+                            <input name="cost" value="{{ $viewData['pokemon']->getCost() }}" type="number" class="form-control">
                         </div>
                     </div>
                 </div>
