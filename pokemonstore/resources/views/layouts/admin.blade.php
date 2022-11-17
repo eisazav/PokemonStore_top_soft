a8bcc254fe1dda152cf65db694d04a876c917f32
mamunetond <47578828+mamunetond@users.noreply.github.com>
2022-11-16 23:34:56 -0500

    Se pulió el admin.blade.php

--- pokemonstore/resources/views/layouts/admin.blade.php
+++ pokemonstore/resources/views/layouts/admin.blade.php
@@ -1,30 +1,55 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') - {{ __('Pokemon Store') }}</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
-  </head>
-  <body class="bg-gray-100 text-gray-800">
-    <nav class="flex py-5 bg-indigo-500 text-white">
-      <div class="w-1/2 px-12 mr-auto">
-        <p class="text-2xl font-bold">
-          <a href="{{ route('admin.home') }}">Panel administrador</a>
-        </p>
-      </div>
-      @include('partials/language_switcher')
-      <div class="vr bg-white mx-2 d-none d-lg-block"></div>
-      @guest
-        <a class="nav-link active" href="{{ route('login') }}">Login</a>
-        <a class="nav-link active" href="{{ route('register') }}">Register</a>
-      @else
-        <form id="logout" action="{{ route('logout') }}" method="POST">
-          <a role="button" class="nav-link active"onclick="document.getElementById('logout').submit();">Logout</a>
-          @csrf
-        </form>
-      @endguest
-    </nav>
-    @yield('content')
-  </body>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrapicons.css"> 
+    <link href="{{ asset('/css/admin.css') }}" rel="stylesheet" />  
+    <title>@yield('title', 'Admin - Pokemon Store')</title>
+</head>
+
+<body>
+    <div class="row g-0">
+        <!-- sidebar -->
+        <div class="p-3 col fixed text-white bg-dark">
+            <a href="{{ route('admin.home.home') }}" class="text-white text-decoration-none">
+                <span class="fs-4">Panel Administration</span>
+            </a>
+            <hr />
+            <ul class="nav flex-column">
+                <li><a href="{{ route('admin.home.home') }}" class="nav-link text-white">- Admin - Home</a></li>
+                <li><a href="{{ route('admin.user.index') }}"  class="nav-link text-white">- Admin - Users</a></li>
+                <li>
+                    <a href="{{ route('home.index') }}" class="mt-2 btn bg-primary text-white">Go back to the home page</a>
+                </li>
+            </ul>
+        </div>
+        <!-- sidebar -->
+        <div class="col content-grey">
+            <nav class="p-3 shadow text-end">
+                <span class="profile-font">Admin</span>
+                <img class="img-profile rounded-circle" src="{{ asset('/img/undraw_profile.svg') }}">
+            </nav>
+            <div class="g-0 m-5">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    <!-- footer -->
+    <div class="copyright py-4 text-center text-white">
+        <div class="container">
+            <small>
+                Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
+                    href="https://twitter.com/danielgarax">
+                    Daniel Correa
+                </a>
+            </small>
+        </div>
+    </div>
+    <!-- footer -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
+</body>
 </html>
