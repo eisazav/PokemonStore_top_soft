3076213da27cfc10e2d1042caa379133743fb069
mamunetond <47578828+mamunetond@users.noreply.github.com>
2022-11-17 01:25:36 -0500

    Repositorio actualizado

--- pokemonstore/database/seeders/DatabaseSeeder.php
+++ pokemonstore/database/seeders/DatabaseSeeder.php
@@ -1,24 +1,31 @@
 <?php
+
 namespace Database\Seeders;
 
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Pokemon;
+use App\Models\User;
+use App\Models\Box;
+use App\Models\BoxItem;
 class DatabaseSeeder extends Seeder
 {
-    /**  
+    /**
      * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
 
-        DB::table('users')->truncate();
-
-        $this->call([ UsersTableSeeder::class]);
-
-        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        User::factory(3)->create();
+        Pokemon::factory(20)->create();
+        Box::factory(4)->create();
+        BoxItem::factory(20)->create();
     }
 }
